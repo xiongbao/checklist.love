fbddc640f47c25b89f4b75598f241cd737e6cb68
xiongbao <yns@qq.com>
2023-10-13 21:07:44 +0800

    feat: version 2.0.0 release

---
+++ core/api.go
@@ -0,0 +1,128 @@
+package core
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
+)
+
+type BlogApi struct {
+	client *githubv4.Client
+	owner  string
+	repo   string
+}
+
+func oauth2Client(accessToken string) *http.Client {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: accessToken},
+	)
+	return oauth2.NewClient(ctx, ts)
+}
+
+func NewApi(username string, repo string, accessToken string) BlogApi {
+
+	return BlogApi{
+		client: githubv4.NewClient(oauth2Client(accessToken)),
+		owner:  username,
+		repo:   repo,
+	}
+}
+
+// FetchPosts 获取 post 列表
+func (api *BlogApi) FetchPosts(before, after, categoryId string) (posts Discussions, err error) {
+
+	var q struct {
+		Resposity struct {
+			Discussion Discussions `graphql:"discussions(first:$discussion_first, orderBy:{field:CREATED_AT,direction:DESC}, after:$after, before:$before, categoryId:$categoryId)"`
+		} `graphql:"repository(owner: $owner, name: $repo)"`
+	}
+
+	binds := map[string]interface{}{
+		"discussion_first": githubv4.Int(PER_PAGE_POST_COUNT),
+		"owner":            githubv4.String(api.owner),
+		"repo":             githubv4.String(api.repo),
+		"after":            (*githubv4.String)(nil),
+		"before":           (*githubv4.String)(nil),
+		"categoryId":       (*githubv4.ID)(nil),
+		"label_first":      githubv4.Int(LABEL_MAX_COUNT),
+	}
+
+	if len(after) > 0 {
+		binds["after"] = (githubv4.String)(after)
+	}
+
+	if len(before) > 0 {
+		binds["before"] = (githubv4.String)(before)
+	}
+
+	if len(categoryId) > 0 {
+		binds["categoryId"] = (githubv4.ID)(categoryId)
+	}
+
+	err = api.client.Query(context.Background(), &q, binds)
+	if err != nil {
+		return Discussions{}, err
+	}
+	posts = q.Resposity.Discussion
+	return posts, nil
+}
+
+// FetchPostComments 根据 id 获取 posts 的所有评论
+func (api *BlogApi) FetchPost(number uint64) (discussion Node, err error) {
+	var q struct {
+		Reposity struct {
+			Discussion Node `graphql:"discussion(number:$number)"`
+		} `graphql:"repository(owner: $owner, name: $repo)"`
+	}
+
+	binds := map[string]interface{}{
+		"number":      githubv4.Int(number),
+		"owner":       githubv4.String(api.owner),
+		"repo":        githubv4.String(api.repo),
+		"label_first": githubv4.Int(LABEL_MAX_COUNT),
+	}
+
+	err = api.client.Query(context.Background(), &q, binds)
+	if err != nil {
+		return Node{}, err
+	}
+	post := q.Reposity.Discussion
+
+	return post, err
+}
+
+// FetchCategories 获取所有的 Category
+func (api *BlogApi) FetchCategories(before string, after string) (Categories, error) {
+	var q struct {
+		Reposity struct {
+			DiscussionCategories Categories `graphql:"discussionCategories(first:$category_first, after:$after, before:$before)"`
+		} `graphql:"repository(owner: $owner, name: $repo)"`
+	}
+
+	binds := map[string]interface{}{
+		"owner":          githubv4.String(api.owner),
+		"repo":           githubv4.String(api.repo),
+		"category_first": githubv4.Int(CATEGORY_MAX_COUNT),
+		"after":          (*githubv4.String)(nil),
+		"before":         (*githubv4.String)(nil),
+	}
+
+	if len(after) > 0 {
+		binds["after"] = (githubv4.String)(after)
+	}
+
+	if len(before) > 0 {
+		binds["before"] = (githubv4.String)(before)
+	}
+
+	err := api.client.Query(context.Background(), &q, binds)
+	if err != nil {
+		return Categories{}, err
+	}
+	categories := q.Reposity.DiscussionCategories
+
+	return categories, err
+}
---
+++ main.go
@@ -0,0 +1,232 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"pure/core"
+	"time"
+
+	cache "github.com/chenyahui/gin-cache"
+	"github.com/chenyahui/gin-cache/persist"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/feeds"
+	"github.com/shurcooL/githubv4"
+	"gopkg.in/yaml.v2"
+)
+
+type Response[T any] struct {
+	Code    int    `json:"code,omitempty"`
+	Data    *T     `json:"data,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+type Category struct {
+	Id   string `yaml:"id"`
+	Name string `yaml:"name"`
+}
+
+type PageQuery struct {
+	Next         string `form:"next,omitempty"`
+	Pre          string `form:"pre,omitempty"`
+	CategoryId   string `uri:"category_id"`
+	CategoryName string `uri:"category_name"`
+}
+
+type PostQuery struct {
+	Id    uint64 `uri:"id" binding:"required"`
+	Title string `uri:"title" binding:"required"`
+}
+
+type PureConfig struct {
+	UserName string `yaml:"username"`
+	Repo     string `yaml:"repo"`
+	RepoId   string `yaml:"repoId"`
+	Website  struct {
+		Host  string `yaml:"host"`
+		Bio   string `yaml:"bio"`
+		Name  string `yaml:"name"`
+		Email string `yaml:"email"`
+	} `yaml:"website"`
+	AccessToken string     `yaml:"accessToken"`
+	Categories  []Category `yaml:"categories"`
+	About       uint64     `yaml:"about"`
+}
+
+var config PureConfig
+var api core.BlogApi
+var memoryCache persist.CacheStore
+
+func init() {
+	configFile, err := os.ReadFile("pure.yaml")
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(configFile, &config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	api = core.NewApi(config.UserName, config.Repo, config.AccessToken)
+	memoryCache = persist.NewMemoryStore(1 * time.Minute)
+}
+
+var funcMap = template.FuncMap{
+	"formatDate": func(unformated githubv4.DateTime) string {
+		return unformated.Time.Format("2006-01-02")
+	},
+	"previewContent": func(fullContent githubv4.String) string {
+		if len([]rune(fullContent)) >= 100 {
+			return string([]rune(fullContent)[:100])
+		}
+		return string(fullContent)
+	},
+	"unescapeHtml": func(bodyHtml githubv4.HTML) template.HTML {
+		return template.HTML(string(bodyHtml))
+	},
+	"isExisted": func(categoryId githubv4.String) bool {
+		for _, category := range config.Categories {
+			if category.Id == string(categoryId) {
+				return true
+			}
+		}
+		return false
+	},
+}
+
+func FetchPosts(c *gin.Context) {
+	var pageQuery PageQuery
+	if err := c.ShouldBind(&pageQuery); err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", map[string]any{
+			"Message": err.Error(),
+		})
+		return
+	}
+
+	if err := c.ShouldBindUri(&pageQuery); err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", map[string]any{
+			"Message": err.Error(),
+		})
+		return
+	}
+
+	categoryId := ""
+
+	if len(pageQuery.CategoryId) > 0 {
+		categoryId = pageQuery.CategoryId
+	}
+
+	// 获取所有文章
+	discussions, err := api.FetchPosts(pageQuery.Pre, pageQuery.Next, categoryId)
+
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", map[string]any{
+			"Message": err.Error(),
+		})
+		return
+	}
+
+	c.HTML(http.StatusOK, "index.html", map[string]any{
+		"Posts":   discussions,
+		"Navbars": config.Categories,
+		"About":   config.About,
+	})
+}
+
+func FetchPost(c *gin.Context) {
+	var postQuery PostQuery
+	if err := c.ShouldBindUri(&postQuery); err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", map[string]any{
+			"Message": err.Error(),
+		})
+		return
+	}
+
+	discussion, err := api.FetchPost(postQuery.Id)
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", map[string]any{
+			"Message": err.Error(),
+		})
+		return
+	}
+
+	c.HTML(http.StatusOK, "post.html", map[string]any{
+		"Post":    discussion,
+		"Navbars": config.Categories,
+		"About":   config.About,
+		"Repo":    fmt.Sprintf("%s/%s", config.UserName, config.Repo),
+		"RepoId":  config.RepoId,
+	})
+}
+
+func AboutPage(c *gin.Context) {
+	discussion, err := api.FetchPost(config.About)
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", map[string]any{
+			"Message": err.Error(),
+		})
+		return
+	}
+
+	c.HTML(http.StatusOK, "post.html", map[string]any{
+		"Post":    discussion,
+		"Navbars": config.Categories,
+		"About":   config.About,
+		"Repo":    fmt.Sprintf("%s/%s", config.UserName, config.Repo),
+		"RepoId":  config.RepoId,
+	})
+}
+
+func GenerateFeed(c *gin.Context) {
+	feed := &feeds.Feed{
+		Title:       config.Website.Name,
+		Link:        &feeds.Link{Href: config.Website.Host},
+		Description: config.Website.Bio,
+		Author:      &feeds.Author{Name: config.Website.Name, Email: config.Website.Email},
+		Created:     time.Now(),
+	}
+
+	discussions, err := api.FetchPosts("", "", config.Categories[0].Id)
+
+	if err != nil {
+		c.XML(http.StatusOK, gin.H{
+			"Message": fmt.Sprintf("Something seems error, please contact %s(%s)", config.Website.Name, config.Website.Email),
+		})
+		return
+	}
+
+	for _, disdiscussion := range discussions.Nodes {
+		feed.Items = append(feed.Items, &feeds.Item{
+			Title:       string(disdiscussion.Title),
+			Description: string([]rune(disdiscussion.Body)[:200]),
+			Author:      &feeds.Author{Name: config.Website.Name, Email: config.Website.Email},
+			Created:     disdiscussion.CreatedAt.Time,
+			Link:        &feeds.Link{Href: fmt.Sprintf("%s/post/%d/%s", config.Website.Host, disdiscussion.Number, disdiscussion.Title)},
+		})
+	}
+
+	feed.WriteAtom(c.Writer)
+}
+
+func main() {
+
+	r := gin.Default()
+	r.SetFuncMap(funcMap)
+	r.LoadHTMLGlob("templates/**/*")
+	r.Static("/assets", "./assets")
+	r.GET("/", cache.CacheByRequestURI(memoryCache, 30*time.Second), FetchPosts)
+	r.GET("/category/:category_id/:category_name", cache.CacheByRequestURI(memoryCache, 30*time.Second), FetchPosts)
+	r.GET("/post/:id/:title", cache.CacheByRequestURI(memoryCache, 1*time.Hour), FetchPost)
+	r.GET("/atom.xml", cache.CacheByRequestURI(memoryCache, 24*time.Hour), GenerateFeed)
+	r.GET("/404", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "error.html", nil)
+	})
+	if config.About > 0 {
+		r.GET("/about", cache.CacheByRequestURI(memoryCache, 1*time.Hour), AboutPage)
+	}
+	r.Run()
+}
